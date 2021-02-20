262bb28ac28078632308d5bf10ed5605486ae6d5
Alessandro Sangalli <sangalli.alessandro@gmail.com>
2021-02-20 00:57:01 -0300

    test: user api e2e tests

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -6,6 +6,46 @@ import { AppModule } from './../src/app.module';
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
+  const userObject = {
+    name: 'any_name',
+    email: 'any_email@email.com',
+  };
+
+  const createUserQuery = () => {
+    return `
+    mutation {
+      createUser(createUserData: {name: "${userObject.name}", email: "${userObject.email}"}) {
+        userId
+        email
+        name
+      }
+    }`;
+  };
+
+  const getUserQuery = () => {
+    return `
+      query {
+        getUsers {
+          userId
+          email
+          name
+        }
+      }
+    `;
+  };
+
+  const updateUserQuery = (userId, newName) => {
+    return `
+      mutation {
+        updateUser(updateUserData: { userId: "${userId}", name: "${newName}" }) {
+        userId
+        email
+        name
+      }
+    }
+    `;
+  };
+
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
@@ -15,10 +55,71 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
+  afterEach(async () => {
+    await app.close();
+  });
+
+  it('should create a user', () => {
     return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+      .post('/graphql')
+      .send({
+        query: createUserQuery(),
+      })
+      .expect(({ body }) => {
+        const data = body.data.createUser;
+        expect(data.name).toBe(userObject.name);
+        expect(data.email).toBe(userObject.email);
+      })
+      .expect(200);
+  });
+
+  it('should get a user', () => {
+    return request(app.getHttpServer())
+      .post('/graphql')
+      .send({
+        query: getUserQuery(),
+      })
+      .expect(({ body }) => {
+        expect(body.data.getUsers.length).toBe(0);
+      })
+      .expect(200);
+  });
+
+  it('should get two users', async () => {
+    await request(app.getHttpServer()).post('/graphql').send({
+      query: createUserQuery(),
+    });
+    await request(app.getHttpServer()).post('/graphql').send({
+      query: createUserQuery(),
+    });
+
+    return request(app.getHttpServer())
+      .post('/graphql')
+      .send({
+        query: getUserQuery(),
+      })
+      .expect(({ body }) => {
+        expect(body.data.getUsers.length).toBe(2);
+      })
+      .expect(200);
+  });
+
+  it('should modify user name', async () => {
+    const result = await request(app.getHttpServer()).post('/graphql').send({
+      query: createUserQuery(),
+    });
+
+    const { userId, name } = result.body.data.createUser;
+    const newName = name + '_new_name';
+
+    return request(app.getHttpServer())
+      .post('/graphql')
+      .send({
+        query: updateUserQuery(userId, newName),
+      })
+      .expect(({ body }) => {
+        expect(newName).toBe(body.data.updateUser.name);
+      })
+      .expect(200);
   });
 });
