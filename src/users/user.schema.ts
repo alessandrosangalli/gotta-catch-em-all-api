cfd3b76a3917b201cbc9352a64f6e0c2048a4f86
Alessandro Sangalli <sangalli.alessandro@gmail.com>
2021-02-21 01:41:57 -0300

    feat: require name+email and make email unique for user

--- src/users/user.schema.ts
+++ src/users/user.schema.ts
@@ -1,6 +1,6 @@
 import * as mongoose from 'mongoose';
 
 export const UserSchema = new mongoose.Schema({
-  name: String,
-  email: String,
+  name: { type: String, unique: false, required: true, dropDups: true },
+  email: { type: String, unique: true, required: true, dropDups: true },
 });
